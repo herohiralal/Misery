5476c1ee15eddec23d205f1532233f794cf50544
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-05-04 19:14:03 +0530

    added natvis support

--- Source/Core/Core._lib.h
+++ Source/Core/Core._lib.h
@@ -4,4 +4,7 @@
 BRAHMA_IMPLEMENT_LIBRARY(Core)
 {
     brahma_append_string_to_paged_list(&library->interfaceDependencies, "__init");
+
+    Brahma_String_KVP natvis = {"Natvis/Core.natvis", "Natvis/Core.natvis"};
+    brahma_append_files_to_copy_to_paged_list(&library->filesToCopyNextToOutput, natvis);
 }
