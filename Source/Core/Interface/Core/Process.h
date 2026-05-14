e2d383dbbf98c2bc47f030f5ff16716b2d9c8101
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-05-14 08:26:46 +0530

    some process stuff

--- Source/Core/Interface/Core/Process.h
+++ Source/Core/Interface/Core/Process.h
@@ -2,6 +2,7 @@
 #include <__init.h>
 #include "Collections.h"
 #include "Stream.h"
+#include "IO.h"
 
 /**
  * Read or Write handle of a pipe.
@@ -67,4 +68,48 @@ struct Process
      * while the `key` and `value` fields are just 'views' into that string.
      */
     static Slice<EnvVarKVP> GetEnvironmentVariables(Allocator allocator);
+
+    #if MSR_WINDOWS
+        using PIDType = DWORD;
+        using ProcessHandleType = HANDLE;
+
+        static constexpr const PIDType k_InvalidPID = (DWORD) -1;
+        static constexpr const ProcessHandleType k_InvalidProcessHandle = INVALID_HANDLE_VALUE;
+    #elif MSR_UNIX
+        using PIDType = pid_t;
+        using ProcessHandleType = pid_t; // on Unix, the PID itself is used as the handle
+
+        static constexpr const PIDType k_InvalidPID = (pid_t) -1;
+        static constexpr const ProcessHandleType k_InvalidProcessHandle = (pid_t) -1;
+    #else
+        #error "Unsupported platform"
+    #endif
+
+    PIDType pid;
+    ProcessHandleType handle;
+
+    Process() = default;
+    Process(PIDType p, ProcessHandleType h) : pid(p), handle(h) { }
+
+    bool IsValid() const { return pid != k_InvalidPID && handle != k_InvalidProcessHandle; }
+    operator bool() const { return IsValid(); }
+
+    /**
+     * Starts a new process with the specified executable and arguments.
+     * Optionally, environment variables, working directory, and pipes for
+     * standard output and error can be provided.
+     *
+     * If not provided, environment variables and working directory are inherited
+     * from the current process. If provided, they must be in a 'KEY=VALUE' format.
+     *
+     * The pipe handles provided must be read ends for stdout and stderr respectively.
+     * If null, the respective output is discarded.
+     */
+    static Process Run(
+        Slice<String> execAndArgs,
+        Slice<String> environmentVariables = Slice<String>(),
+        DirectoryPath workingDirectory = DirectoryPath(String()),
+        PipeHandle* stdOutPipe = nullptr,
+        PipeHandle* stdErrPipe = nullptr
+    );
 };
--- Source/Core/Internal/Process.cpp
+++ Source/Core/Internal/Process.cpp
@@ -1,4 +1,5 @@
 #include <Core/Process.h>
+#include <Core/Defer.h>
 
 bool PipeHandle::Create(PipeHandle* outR, PipeHandle* outW)
 {
@@ -150,4 +151,289 @@ void PipeHandle::Close()
     #endif
 
     handle = k_InvalidHandle;
+}
+
+void Process::Exit(int32_t exitCode)
+{
+    #if PNSLR_WINDOWS
+    {
+        ExitProcess((UINT) exitCode);
+    }
+    #elif PNSLR_UNIX
+    {
+        // On Unix-like systems, we can use the exit system call directly.
+        _exit(exitCode);
+    }
+    #endif
+}
+
+// in unicode, on windows, GetEnvironmentStringsA is not even declared
+// so declaring this here to ensure we can call it regardless of UNICODE being defined or not
+#if defined(UNICODE) && defined(_WIN32)
+#undef GetEnvironmentStrings
+LPCH GetEnvironmentStringsA(void) { return GetEnvironmentStrings(); }
+#define GetEnvironmentStrings GetEnvironmentStringsW
+#endif
+
+Slice<EnvVarKVP> Process::GetEnvironmentVariables(Allocator allocator)
+{
+    Slice<EnvVarKVP> envVars = Slice<EnvVarKVP>();
+
+    size_t envVarsCount = 0;
+
+#if MSR_WINDOWS
+
+    LPCH envStringsWindows = GetEnvironmentStringsA();
+    if (!envStringsWindows) { return envVars; }
+
+    DEFER { FreeEnvironmentStringsA(envStringsWindows); };
+
+    for (LPCH var = envStringsWindows; *var; var += strlen(var) + 1)
+        envVarsCount++;
+
+#elif MSR_UNIX
+
+    for (cstring* var = environ; *var; var++)
+        envVarsCount++;
+
+#else
+        #error "unsupported platform"
+#endif
+
+    envVars = allocator.MakeSlice<EnvVarKVP>(envVarsCount, SRC_LOC());
+    if (!envVars) return envVars;
+
+    int64_t index = 0;
+
+    size_t fullLen = 0;
+#if MSR_WINDOWS
+    for (LPCH var = envStringsWindows; *var; var += fullLen + 1)
+    {
+#elif MSR_UNIX
+    for (char** varPtr = environ; *varPtr; varPtr++)
+    {
+        char* var = *varPtr;
+#else
+        #error "unsupported platform"
+#endif
+
+        fullLen = strlen(var);
+        if (fullLen == 0) { continue; }
+
+        char* equalSign = strchr(var, '=');
+        if (!equalSign)       { continue; } // malformed entry, skip
+        if (equalSign == var) { continue; } // empty key, skip
+
+        String kvp = allocator.CloneString(String(reinterpret_cast<uint8_t*>(var), fullLen), SRC_LOC());
+
+        size_t keyLen = (size_t) (strchr((char*) kvp.Data(), '=') - (char*) kvp.Data());
+        size_t valLen = kvp.Length() - keyLen - 1; // -1 for '='
+
+        EnvVarKVP& kvpEntry = envVars[index];
+        kvpEntry.kvp   = kvp;
+        kvpEntry.key   = kvp.SubString(0,          keyLen);
+        kvpEntry.value = kvp.SubString(keyLen + 1, valLen);
+        index++;
+
+#if MSR_WINDOWS
+    }
+#elif MSR_UNIX
+    }
+#else
+        #error "unsupported platform"
+#endif
+
+    envVars = envVars.SubSlice(0, index); // in case of malformed entries
+    return envVars;
+}
+
+namespace Misery::Internal::Process
+{
+#if MSR_WINDOWS
+
+    CString BuildWindowsProcessCmdLine(Slice<String> execAndArgs)
+    {
+        List<char> sb = alloc_temp.MakeList<char>();
+
+        size_t minLen = 25; // adjusting for maybe 25 chars of extra backslashes and the null terminator
+        for (const auto& arg : execAndArgs)
+            minLen += arg.Length() + 3; // +3 for potential quotes and space
+
+        sb.Reserve(minLen, SRC_LOC());
+
+        for (size_t i = 0; i < execAndArgs.Count(); i++)
+        {
+            String arg = execAndArgs[i];
+            if (i != 0) { sb.Add(' ', SRC_LOC()); }
+
+            bool needsQuotes = false;
+            for (size_t j = 0; j < arg.Length(); j++)
+            {
+                if (false ||
+                    arg[j] == '('  ||
+                    arg[j] == ')'  ||
+                    arg[j] == '['  ||
+                    arg[j] == ']'  ||
+                    arg[j] == '{'  ||
+                    arg[j] == '}'  ||
+                    arg[j] == '^'  ||
+                    arg[j] == '='  ||
+                    arg[j] == ';'  ||
+                    arg[j] == '!'  ||
+                    arg[j] == '\'' ||
+                    arg[j] == '+'  ||
+                    arg[j] == ','  ||
+                    arg[j] == '`'  ||
+                    arg[j] == '~'  ||
+                    arg[j] == '\"' ||
+                    arg[j] == ' '  ||
+                    arg[j] == '\t' || // include tab as whitespace
+                    false)
+                {
+                    needsQuotes = true;
+                    break;
+                }
+            }
+
+            if (!needsQuotes)
+            {
+                for (size_t j = 0; j < arg.Length(); j++)
+                    sb.Add(arg[j], SRC_LOC());
+                continue;
+            }
+
+            sb.Add('\"', SRC_LOC()); // start
+            for (size_t j = 0; j < arg.Length(); j++)
+            {
+                size_t backslashes = 0;
+
+                while (j < arg.Length() && arg[j] == '\\') { backslashes++; j++; }
+
+                if (j == arg.Length())
+                {
+                    // Escape all backslashes at the end
+                    for (size_t k = 0; k < backslashes * 2; k++)
+                        sb.Add('\\', SRC_LOC());
+                    break;
+                }
+                else if (arg[j] == '\"')
+                {
+                    // Escape all backslashes and the quote
+                    for (size_t k = 0; k < (backslashes * 2) + 1; k++)
+                        sb.Add('\\', SRC_LOC());
+
+                    sb.Add('\"', SRC_LOC());
+                }
+                else
+                {
+                    // No special handling needed, just output the backslashes
+                    for (size_t k = 0; k < backslashes; k++)
+                        sb.Add('\\', SRC_LOC());
+
+                    sb.Add(arg[j], SRC_LOC());
+                }
+            }
+            sb.Add('\"', SRC_LOC()); // end
+        }
+
+        sb.Add('\0', SRC_LOC());
+        return CString(sb.Data());
+    }
+
+    CString BuildWindowsProcessEnvBlock(Slice<String> envVars)
+    {
+        List<char> sb = alloc_temp.MakeList<char>();
+
+        size_t minLen = 0;
+        for (const auto& kv : envVars)
+            minLen += kv.Length() + 1; // +1 for null terminator
+        minLen += 1; // for the double null terminator
+        sb.Reserve(minLen, SRC_LOC());
+
+        for (int64_t currIdx = (int64_t) envVars.Count() - 1; currIdx >= 0; --currIdx)
+        {
+            String kv = envVars[currIdx];
+
+            int64_t eqIdx = kv.FirstIndexOf("=");
+            if (eqIdx == -1) { continue; } // malformed, skip
+
+            String key = kv.SubString(0, eqIdx);
+            String value = kv.SubString(eqIdx + 1, kv.Length() - eqIdx - 1);
+
+            bool foundDuplicate = false;
+            for (int64_t prevIdx = (currIdx + 1); prevIdx < envVars.Count(); prevIdx++)
+            {
+                int64_t prevEqIdx = envVars[prevIdx].FirstIndexOf("=");
+                if (prevEqIdx == -1) { continue; } // malformed, skip
+
+                String prevKey = envVars[prevIdx].SubString(0, prevEqIdx);
+                if (prevKey == key) { foundDuplicate = true; break; }
+            }
+
+            if (foundDuplicate) { continue; } // skip this one, a later one exists
+            for (size_t j = 0; j < kv.Length(); j++)
+                sb.Add(kv[j], SRC_LOC());
+            sb.Add('\0', SRC_LOC()); // null terminator
+        }
+        sb.Add('\0', SRC_LOC()); // double-null-terminate the block
+        return CString(sb.Data());
+    }
+
+#elif MSR_UNIX
+
+    Slice<String> SplitUnixPathList(String pathStr)
+    {
+        if (!pathStr) { return Slice<String>(); }
+
+        size_t start = 0, count = 0;
+        bool quote = false;
+
+        for (size_t i = 0; i < pathStr.Length(); i++)
+        {
+            uint8_t c = pathStr[i];
+            if (c == '\"') { quote = !quote; }
+            else if (c == ':' && !quote) { count++; }
+        }
+
+        start = 0;
+        quote = false;
+
+        auto paths = alloc_temp.MakeSlice<String>(count + 1, SRC_LOC());
+        if (!paths) { return Slice<String>(); }
+
+        size_t idx = 0;
+        for (size_t i = 0; i < pathStr.Length(); i++)
+        {
+            uint8_t c = pathStr[i];
+            if (c == '\"') { quote = !quote; }
+            else if (c == ':' && !quote)
+            {
+                paths[idx++] = pathStr.SubString(start, i - start);
+                start = i + 1;
+            }
+        }
+        MSR_ASSERT(idx == count && "Count mismatch in SplitUnixPathList");
+
+        paths[idx] = pathStr.SubString(start, pathStr.Length() - start);
+
+        for (size_t i = 0; i < paths.Count(); i++)
+        {
+            String p = paths[i];
+            if (p.Length() >= 2 && p[0] == '\"' && p[p.Length() - 1] == '\"')
+                paths[i] = p.SubString(1, p.Length() - 2);
+        }
+
+        return paths;
+    }
+
+#endif
+}
+
+Process Process::Run(
+    Slice<String> execAndArgs,
+    Slice<String> environmentVariables,
+    DirectoryPath workingDirectory,
+    PipeHandle* stdOutPipe,
+    PipeHandle* stdErrPipe)
+{
 }
