65e1fe9dc66927f1f5340697a0eaad9e2105fac3
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-01 11:02:03 +0530

    some setup to do fmt

--- Source/Core/Interface/Core/Strings.h
+++ Source/Core/Interface/Core/Strings.h
@@ -157,4 +157,216 @@ STR_EncodedRune STR_EncodeRune(u32 c);
  */
 STR_DecodedRune STR_DecodeRune(utf8str s);
 
+// formatting ------------------------------------------------------------------------------------------------------------------
+
+/**
+ * The possible primitive types that can be formatted.
+ */
+typedef u8 FMT_Ty;
+enum FMT_Tys
+{
+    FMT_Ty_Ukn,
+    FMT_Ty_Int,
+    FMT_Ty_Dbl,
+    FMT_Ty_Str,
+    FMT_Ty_Ptr,
+};
+
+/**
+ * The base to use when formatting an integer argument.
+ */
+typedef u8 FMT_IntBase;
+enum FMT_IntBases
+{
+    FMT_IntBase_Dec,
+    FMT_IntBase_Hex,
+    FMT_IntBase_Bin,
+    FMT_IntBase_Oct,
+};
+
+/**
+ * Different modifiers to use when formatitng a string argument.
+ */
+typedef u8 FMT_StrStyle;
+enum FMT_StrStyles
+{
+    FMT_StrStyle_Default               =      0,
+    FMT_StrStyle_AllCaps               = 1 << 0,
+    FMT_StrStyle_NoCaps                = 1 << 1,
+    FMT_StrStyle_NoSpaces              = 1 << 2,
+    FMT_StrStyle_FwdSlashesOnly        = 1 << 3, // replace backslashes with forward slashes
+    FMT_StrStyle_BwdSlashesOnly        = 1 << 4, // replace forward slashes with backslashes
+    FMT_StrStyle_RemoveMultipleSlashes = 1 << 5, // replace multiple consecutive slashes with a single slash
+};
+
+/**
+ * The internal encoding of a type-unspecific format specifier.
+ */
+typedef union
+{
+    FMT_Ty fmtTy;
+
+    struct
+    {
+        FMT_Ty fmtTy;
+        b8 isNegative;
+        FMT_IntBase base;
+        u64 value;
+    } intVal;
+
+    struct
+    {
+        FMT_Ty fmtTy;
+        u16 minDecimalPlaces;
+        u16 maxDecimalPlaces;
+        double value;
+    } dblVal;
+
+    struct
+    {
+        FMT_Ty fmtTy;
+        FMT_StrStyle format;
+        utf8str value;
+    } strVal;
+
+    struct
+    {
+        FMT_Ty fmtTy;
+        const void* value;
+    } ptrVal;
+} FMT_Arg;
+
+COL_DECLARE_FOR(FMT_Arg)
+
+FMT_Arg FMT_B8(b8 v);
+
+FMT_Arg FMT_U64(u64 v, FMT_IntBase base OPT_ARG);
+FMT_Arg FMT_U32(u32 v, FMT_IntBase base OPT_ARG);
+FMT_Arg FMT_U16(u16 v, FMT_IntBase base OPT_ARG);
+FMT_Arg FMT_U8(u8 v, FMT_IntBase base OPT_ARG);
+
+FMT_Arg FMT_I64(i64 v, FMT_IntBase base OPT_ARG);
+FMT_Arg FMT_I32(i32 v, FMT_IntBase base OPT_ARG);
+FMT_Arg FMT_I16(i16 v, FMT_IntBase base OPT_ARG);
+FMT_Arg FMT_I8(i8 v, FMT_IntBase base OPT_ARG);
+
+#ifdef __cplusplus
+FMT_Arg FMT_F64(f64 v, u16 minDecimalPlaces = 0, u16 maxDecimalPlaces = 6);
+FMT_Arg FMT_F32(f32 v, u16 minDecimalPlaces = 0, u16 maxDecimalPlaces = 6);
+#else
+FMT_Arg FMT_F64(f64 v, u16 minDecimalPlaces, u16 maxDecimalPlaces);
+FMT_Arg FMT_F32(f32 v, u16 minDecimalPlaces, u16 maxDecimalPlaces);
+#endif
+
+FMT_Arg FMT_Str(utf8str str, FMT_StrStyle style OPT_ARG);
+FMT_Arg FMT_CStr(cstring str, FMT_StrStyle style OPT_ARG);
+
+FMT_Arg FMT_Ptr(const void* ptr);
+
+#ifdef __cplusplus
+    EXTERN_C_END
+
+    template <typename T>
+    static inline FMT_Arg FMT_Generic(T v) { return FMT_Arg { }; }
+
+    #define DECLARE_FMT_SPEC(fnSuffix, ty) \
+        template <> static inline FMT_Arg FMT_Generic(ty v) { return FMT_##fnSuffix(v); }
+
+    DECLARE_FMT_SPEC(B8,   b8)
+    DECLARE_FMT_SPEC(U8,   u8)
+    DECLARE_FMT_SPEC(U16,  u16)
+    DECLARE_FMT_SPEC(U32,  u32)
+    DECLARE_FMT_SPEC(U64,  u64)
+    DECLARE_FMT_SPEC(I8,   i8)
+    DECLARE_FMT_SPEC(I16,  i16)
+    DECLARE_FMT_SPEC(I32,  i32)
+    DECLARE_FMT_SPEC(I64,  i64)
+    DECLARE_FMT_SPEC(F32,  f32)
+    DECLARE_FMT_SPEC(F64,  f64)
+    DECLARE_FMT_SPEC(Str,  utf8str)
+    DECLARE_FMT_SPEC(CStr, cstring)
+    DECLARE_FMT_SPEC(Ptr,  const void*)
+    DECLARE_FMT_SPEC(Ptr,  rawptr)
+    DECLARE_FMT_SPEC(Ptr,  std::nullptr_t)
+
+    #undef DECLARE_FMT_SPEC
+
+    #define FMT(x) (FMT_Generic(x))
+
+    EXTERN_C_BEGIN
+#else
+
+    typedef enum
+    {
+        FMT_CTy_Unknown,
+        FMT_CTy_B8,
+        FMT_CTy_U8,
+        FMT_CTy_U16,
+        FMT_CTy_U32,
+        FMT_CTy_U64,
+        FMT_CTy_I8,
+        FMT_CTy_I16,
+        FMT_CTy_I32,
+        FMT_CTy_I64,
+        FMT_CTy_F32,
+        FMT_CTy_F64,
+        FMT_CTy_Str,
+        FMT_CTy_CStr,
+        FMT_CTy_Ptr,
+    } FMT_CTys;
+
+    static inline FMT_Arg FMT_Generic(FMT_CTys ty, ...)
+    {
+        FMT_Arg o = {0};
+
+        va_list l;
+        va_start(l, ty);
+        switch (ty)
+        {
+            case FMT_CTy_B8:   { i32     v = va_arg(l,     i32); o =   FMT_B8((b8)  v      ); break; }
+            case FMT_CTy_U8:   { i32     v = va_arg(l,     i32); o =   FMT_U8((u8)  v, 0   ); break; }
+            case FMT_CTy_U16:  { i32     v = va_arg(l,     i32); o =  FMT_U16((u16) v, 0   ); break; }
+            case FMT_CTy_U32:  { u32     v = va_arg(l,     u32); o =  FMT_U32(      v, 0   ); break; }
+            case FMT_CTy_U64:  { u64     v = va_arg(l,     u64); o =  FMT_U64(      v, 0   ); break; }
+            case FMT_CTy_I8:   { i32     v = va_arg(l,     i32); o =   FMT_I8((i8)  v, 0   ); break; }
+            case FMT_CTy_I16:  { i32     v = va_arg(l,     i32); o =  FMT_I16((i16) v, 0   ); break; }
+            case FMT_CTy_I32:  { i32     v = va_arg(l,     i32); o =  FMT_I32(      v, 0   ); break; }
+            case FMT_CTy_I64:  { i64     v = va_arg(l,     i64); o =  FMT_I64(      v, 0   ); break; }
+            case FMT_CTy_F32:  { f64     v = va_arg(l,     f64); o =  FMT_F32((f32) v, 0, 6); break; }
+            case FMT_CTy_F64:  { f64     v = va_arg(l,     f64); o =  FMT_F64(      v, 0, 6); break; }
+            case FMT_CTy_Str:  { utf8str v = va_arg(l, utf8str); o =  FMT_Str(      v, 0   ); break; }
+            case FMT_CTy_CStr: { cstring v = va_arg(l, cstring); o = FMT_CStr(      v, 0   ); break; }
+            case FMT_CTy_Ptr:  { rawptr  v = va_arg(l,  rawptr); o =  FMT_Ptr(      v      ); break; }
+            case FMT_CTy_Unknown:
+            default:
+                break;
+        }
+        va_end(l);
+
+        return o;
+    }
+
+    #define FMT(x) \
+        (_Generic((x), \
+            b8:          FMT_Generic(FMT_CTy_B8,   (x)), \
+            u8:          FMT_Generic(FMT_CTy_U8,   (x)), \
+            u16:         FMT_Generic(FMT_CTy_U16,  (x)), \
+            u32:         FMT_Generic(FMT_CTy_U32,  (x)), \
+            u64:         FMT_Generic(FMT_CTy_U64,  (x)), \
+            i8:          FMT_Generic(FMT_CTy_I8,   (x)), \
+            i16:         FMT_Generic(FMT_CTy_I16,  (x)), \
+            i32:         FMT_Generic(FMT_CTy_I32,  (x)), \
+            i64:         FMT_Generic(FMT_CTy_I64,  (x)), \
+            utf8str:     FMT_Generic(FMT_CTy_Str,  (x)), \
+            cstring:     FMT_Generic(FMT_CTy_CStr, (x)), \
+            char*:       FMT_Generic(FMT_CTy_CStr, (x)), \
+            const void*: FMT_Generic(FMT_CTy_Ptr,  (x)), \
+            rawptr:      FMT_Generic(FMT_CTy_Ptr,  (x)), \
+            default:     FMT_Generic(FMT_CTy_Unknown  )  \
+        ))
+
+#endif
+
+#define FMTARGS(...) SLICE(FMT_Arg, __VA_ARGS__)
+
 EXTERN_C_END
--- Source/Core/Internal/Strings.c
+++ Source/Core/Internal/Strings.c
@@ -36,7 +36,7 @@ utf8str STR_Join(utf8str str1, utf8str str2, MEM_Allocator allocator)
     return str;
 }
 
-utf8str STR_ToUpper(utf8str str, MEM_Allocator allocator OPT_ARG)
+utf8str STR_ToUpper(utf8str str, MEM_Allocator allocator)
 {
     if (!str.data || !str.count) return (utf8str) {0};
 
@@ -53,7 +53,7 @@ utf8str STR_ToUpper(utf8str str, MEM_Allocator allocator OPT_ARG)
     return str;
 }
 
-utf8str STR_ToLower(utf8str str, MEM_Allocator allocator OPT_ARG)
+utf8str STR_ToLower(utf8str str, MEM_Allocator allocator)
 {
     if (!str.data || !str.count) return (utf8str) {0};
 
@@ -126,7 +126,7 @@ b8 STR_HasSuffixIgnoreCase(utf8str str, utf8str suffix)
     return STR_EqIgnoreCase(suffixStr, suffix);
 }
 
-isize STR_Find(utf8str str, utf8str subString, b8 ignoreCase OPT_ARG)
+isize STR_Find(utf8str str, utf8str subString, b8 ignoreCase)
 {
     if (!str.data || !str.count || !subString.data || !subString.count)
         return -1;
@@ -145,7 +145,7 @@ isize STR_Find(utf8str str, utf8str subString, b8 ignoreCase OPT_ARG)
     return -1;
 }
 
-isize STR_FindLast(utf8str str, utf8str subString, b8 ignoreCase OPT_ARG)
+isize STR_FindLast(utf8str str, utf8str subString, b8 ignoreCase)
 {
     if (!str.data || !str.count || !subString.data || !subString.count)
         return -1;
@@ -404,3 +404,123 @@ STR_DecodedRune STR_DecodeRune(utf8str s)
 #undef STR_RUNE3_MAX
 #undef STR_LOCB
 #undef STR_HICB
+
+FMT_Arg FMT_B8(b8 v)
+{
+    return (FMT_Arg)
+    {
+        .strVal =
+        {
+            .fmtTy  = FMT_Ty_Str,
+            .value  = v ? UTF8STR("true") : UTF8STR("false"),
+            .format = FMT_StrStyle_Default,
+        },
+    };
+}
+
+FMT_Arg FMT_U64(u64 v, FMT_IntBase base)
+{
+    return (FMT_Arg)
+    {
+        .intVal =
+        {
+            .fmtTy = FMT_Ty_Int,
+            .value = v,
+            .isNegative = false,
+            .base  = base,
+        },
+    };
+}
+
+FMT_Arg FMT_U32(u32 v, FMT_IntBase base)
+{
+    return FMT_U64((u64) v, base);
+}
+
+FMT_Arg FMT_U16(u16 v, FMT_IntBase base)
+{
+    return FMT_U64((u64) v, base);
+}
+
+FMT_Arg FMT_U8(u8 v, FMT_IntBase base)
+{
+    return FMT_U64((u64) v, base);
+}
+
+FMT_Arg FMT_I64(i64 v, FMT_IntBase base)
+{
+    return (FMT_Arg)
+    {
+        .intVal =
+        {
+            .fmtTy = FMT_Ty_Int,
+            .value = (v == I64_MIN) ? (1LL + (u64) I64_MAX) : (u64) ((v < 0) ? -v : v),
+            .isNegative = (v < 0),
+            .base  = base,
+        },
+    };
+}
+
+FMT_Arg FMT_I32(i32 v, FMT_IntBase base)
+{
+    return FMT_I64((i64) v, base);
+}
+
+FMT_Arg FMT_I16(i16 v, FMT_IntBase base)
+{
+    return FMT_I64((i64) v, base);
+}
+
+FMT_Arg FMT_I8(i8 v, FMT_IntBase base)
+{
+    return FMT_I64((i64) v, base);
+}
+
+FMT_Arg FMT_F64(f64 v, u16 minDecimalPlaces, u16 maxDecimalPlaces)
+{
+    return (FMT_Arg)
+    {
+        .dblVal =
+        {
+            .fmtTy = FMT_Ty_Dbl,
+            .value = v,
+            .minDecimalPlaces = minDecimalPlaces,
+            .maxDecimalPlaces = maxDecimalPlaces,
+        },
+    };
+}
+
+FMT_Arg FMT_F32(f32 v, u16 minDecimalPlaces, u16 maxDecimalPlaces)
+{
+    return FMT_F64((f64) v, minDecimalPlaces, maxDecimalPlaces);
+}
+
+FMT_Arg FMT_Str(utf8str str, FMT_StrStyle style)
+{
+    return (FMT_Arg)
+    {
+        .strVal =
+        {
+            .fmtTy  = FMT_Ty_Str,
+            .value  = str,
+            .format = style,
+        },
+    };
+}
+
+FMT_Arg FMT_CStr(cstring str, FMT_StrStyle style)
+{
+    return FMT_Str(STR_StringFromCStr(str), style);
+}
+
+FMT_Arg FMT_Ptr(const void* ptr)
+{
+    return (FMT_Arg)
+    {
+        .ptrVal =
+        {
+            .fmtTy  = FMT_Ty_Ptr,
+            .value  = ptr,
+        },
+    };
+}
