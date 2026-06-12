1368ae488fbc428fc7186e9ad4f2902b535f424c
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-12 18:34:49 +0530

    renderer skeleton

---
+++ Source/Renderer/Interface/Renderer/Renderer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Core/Core.h>
+#include <Platform/Platform.h>
+#include <RendererBase/RendererBase.h>
+
+EXTERN_C_BEGIN
+
+EXTERN_C_END
---
+++ Source/RendererBase/Interface/RendererBase/RendererBase.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <Core/Core.h>
+#include <Platform/Platform.h>
+
+EXTERN_C_BEGIN
+
+EXTERN_C_END
