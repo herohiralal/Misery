1066f9d146f9b9fb5f838b77eaeea56e8247e2c1
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-16 00:55:41 +0530

    disabled dx12/mtl

--- Source/Misery._pkg.h
+++ Source/Misery._pkg.h
@@ -5,4 +5,12 @@
 BRAHMA_IMPLEMENT_PACKAGE(Misery)
 {
     package->primaryLibrary = "CoreTest";
+
+    // TODO: implement
+    Brahma_String_KVP disableDx12 = {"REN_DX12", "0"};
+    brahma_append_definition_to_paged_list(&package->definitions, disableDx12);
+
+    // TODO: implement
+    Brahma_String_KVP disableMtl = {"REN_MTL", "0"};
+    brahma_append_definition_to_paged_list(&package->definitions, disableMtl);
 }
