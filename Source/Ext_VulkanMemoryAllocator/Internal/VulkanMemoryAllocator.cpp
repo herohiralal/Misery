48473dfb8e2c1240b0a84a322ca490c530267cb7
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-15 22:42:55 +0530

    warning thing

--- Source/Ext_VulkanMemoryAllocator/Internal/VulkanMemoryAllocator.cpp
+++ Source/Ext_VulkanMemoryAllocator/Internal/VulkanMemoryAllocator.cpp
@@ -1,7 +1,9 @@
 #include <VulkanLoader.h>
 
 #if REN_VK
+    MSR_SUPPRESS_WARN
     #define VMA_IMPLEMENTATION 1
     #include <VulkanMemoryAllocator.h>
     #undef VMA_IMPLEMENTATION
+    MSR_UNSUPPRESS_WARN
 #endif
