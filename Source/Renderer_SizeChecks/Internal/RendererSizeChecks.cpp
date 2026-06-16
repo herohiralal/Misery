cbc92530774b3b290991c5e6ce8db51d0c35aaed
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-16 08:24:31 +0530

    improved size check message

--- Source/Renderer_SizeChecks/Internal/RendererSizeChecks.cpp
+++ Source/Renderer_SizeChecks/Internal/RendererSizeChecks.cpp
@@ -1,5 +1,14 @@
 #include <__init.h>
 
+template <bool condition>
+struct REN_TNoValueOnFailure { };
+
+template <>
+struct REN_TNoValueOnFailure<true> { static constexpr const bool VALUE = true; };
+
+#define REN_OBJ_SIZE_CHECK_NAME(name) \
+    ______________________________________________________IF_YOU_SEE_THIS_LINE_IN_YOUR_COMPILER_ERROR_THEN_SET_THE_PADDING_OF_TYPE_____REN_##name##_____TO_
+
 #define REN_OBJ_SIZE_CHECK(gfxApi, name) \
     static_assert( \
         alignof(REN_##name) % alignof(REN_##gfxAPi##name) == 0, \
@@ -8,11 +17,13 @@
     ); \
     EXTERN_C_END \
     template <std::size_t N> \
-    struct ______________________________________________________________IF_YOU_SEE_THIS_LINE_IN_YOUR_COMPILER_ERROR_THEN_SET_THE_PADDING_OF_TYPE_____REN_##name##_____TO_THE_VALUE_IN_ANGULAR_BRACKETS \
+    struct REN_OBJ_SIZE_CHECK_NAME(name) \
+        : public REN_TNoValueOnFailure<(sizeof(REN_##name) >= sizeof(REN_##gfxApi##name))> \
     { \
-        static_assert(sizeof(REN_##name) >= sizeof(REN_##gfxApi##name), ""); \
     }; \
-    template struct ______________________________________________________________IF_YOU_SEE_THIS_LINE_IN_YOUR_COMPILER_ERROR_THEN_SET_THE_PADDING_OF_TYPE_____REN_##name##_____TO_THE_VALUE_IN_ANGULAR_BRACKETS<sizeof(REN_##gfxApi##name)>; \
+    static_assert(REN_OBJ_SIZE_CHECK_NAME(name)<sizeof(REN_##gfxApi##name)>::VALUE, \
+        "The size of REN_" #name " must be greater than or equal to the size of REN_" #gfxApi #name "." \
+    ); \
     EXTERN_C_BEGIN
 
 #include <Renderer_Base/Renderer_Base.h>
@@ -20,3 +31,6 @@
 #include <Renderer_Vk/Renderer_Vk.h>
 #include <Renderer_Dx12/Renderer_Dx12.h>
 #include <Renderer_Mtl/Renderer_Mtl.h>
+
+#undef REN_OBJ_SIZE_CHECK
+#undef REN_OBJ_SIZE_CHECK_NAME
