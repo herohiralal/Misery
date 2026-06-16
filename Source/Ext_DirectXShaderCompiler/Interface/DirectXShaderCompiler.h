98e355265b07c3e13443a59074b6ac3eae22a55a
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-16 07:04:06 +0530

    warn fixes

--- Source/Ext_DirectXShaderCompiler/Interface/DirectXShaderCompiler.h
+++ Source/Ext_DirectXShaderCompiler/Interface/DirectXShaderCompiler.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <__init.h>
 
+MSR_SUPPRESS_WARN
 #if MSR_WINDOWS || MSR_LINUX
     #ifdef __cplusplus
-        #include "Unknwn.h"
+        #if MSR_WINDOWS
+            #include "Unknwn.h"
+        #endif
         #include "ExtDeps/dxc/dxcapi.h"
         #define DX_SHADER_COMPILER 1
     #endif
 #endif
+MSR_UNSUPPRESS_WARN
 
 #ifndef DX_SHADER_COMPILER
     #define DX_SHADER_COMPILER 0
