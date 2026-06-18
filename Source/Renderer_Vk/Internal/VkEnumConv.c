7a79ea560d2d122a161c647e0e3f42bd0986063a
Rohan Jadav <ro1.darkstar7@gmail.com>
2026-06-19 01:33:45 +0530

    some vk fns

---
+++ Source/Renderer_Vk/Internal/VkEnumConv.c
@@ -0,0 +1,41 @@
+#include "VkPrivate.h"
+
+#if REN_VK
+
+inline VkFormat REN_BreakVkTextureFormat(REN_TextureFormat fmt)
+{
+    switch ((enum REN_TextureFormats) fmt)
+    {
+        case REN_TexFmt_Unknown:            return VK_FORMAT_UNDEFINED;
+        case REN_TexFmt_D32_Float:          return VK_FORMAT_D32_SFLOAT;
+        case REN_TexFmt_R8G8B8A8_UNorm:     return VK_FORMAT_R8G8B8A8_UNORM;
+        case REN_TexFmt_B8G8R8A8_UNorm:     return VK_FORMAT_B8G8R8A8_UNORM;
+        case REN_TexFmt_R16G16B16A16_UNorm: return VK_FORMAT_R16G16B16A16_UNORM;
+    }
+
+    return VK_FORMAT_UNDEFINED;
+}
+
+inline REN_TextureFormat REN_MakeVkTextureFormat(VkFormat fmt)
+{
+    MSR_SUPPRESS_WARN // the enum has like 250+ cases...
+    #ifdef __clang__
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Wswitch"
+    #endif
+    switch (fmt)
+    {
+        case VK_FORMAT_D32_SFLOAT:         return REN_TexFmt_D32_Float;
+        case VK_FORMAT_R8G8B8A8_UNORM:     return REN_TexFmt_R8G8B8A8_UNorm;
+        case VK_FORMAT_B8G8R8A8_UNORM:     return REN_TexFmt_B8G8R8A8_UNorm;
+        case VK_FORMAT_R16G16B16A16_UNORM: return REN_TexFmt_R16G16B16A16_UNorm;
+    }
+    #ifdef __clang__
+        #pragma clang diagnostic pop
+    #endif
+    MSR_UNSUPPRESS_WARN
+
+    return REN_TexFmt_Unknown;
+}
+
+#endif
